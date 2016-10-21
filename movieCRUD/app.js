76155ca36c206d2c213eacf9b78b15bb8ea43972
luunguyen <luunguyen@ccintegration.com>
2016-10-21 17:52:20 +0700

    sadasd

--- movieCRUD/app.js
+++ movieCRUD/app.js
@@ -16,7 +16,7 @@ var app = express();
 
 var dbName='movieDB';
 
-var connectionString='mongodb://stdev:stdev@localhost:27017/'+dbName ;
+var connectionString='mongodb://stdev:stdev@172.16.4.106:27017/'+dbName ;
 mongoose.connect(connectionString,{auth:{authdb:"admin"}});
 
 
