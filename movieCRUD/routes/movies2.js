dfda18ae83feda6b5b96439983d8abe46b3239ff
luunguyen <luunguyen@ccintegration.com>
2016-10-21 16:43:17 +0700

    Update

---
+++ movieCRUD/routes/movies2.js
@@ -0,0 +1,72 @@
+/**
+ * Created by Sandeep on 01/06/14.
+ */
+
+var Movie=require('../models/movie');
+var express=require('express');
+
+//configure routes
+
+var router=express.Router();
+
+router.route('/movies1')
+    .get(function(req,res){
+       Movie.find(function(err,movies1){
+           if(err)
+                res.send(err);
+           res.json(movies1);
+       });
+    })
+
+    .post(function(req,res){
+        var movie=new Movie(req.body);
+        movie.save(function(err){
+            if(err)
+                res.send(err);
+            res.send({message:'Movie Added'});
+        });
+    });
+
+router.route('/movies1/:id')
+    .put(function(req,res){
+        Movie.findOne({_id:req.params.id},function(err,movie){
+
+            if(err)
+                res.send(err);
+
+           for(prop in req.body){
+                movie[prop]=req.body[prop];
+           }
+
+            // save the movie
+            movie.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Movie updated!' });
+            });
+
+        });
+    })
+
+    .get(function(req,res){
+        Movie.findOne({_id:req.params.id},function(err, movie) {
+            if(err)
+                res.send(err);
+
+            res.json(movie);
+        });
+    })
+
+    .delete(function(req,res){
+        Movie.remove({
+            _id: req.params.id
+        }, function(err, movie) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+module.exports=router;
