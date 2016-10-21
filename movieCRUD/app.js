e36bb1bc53061e76965b9f91691b058da46a5370
luunguyen <luunguyen@ccintegration.com>
2016-10-21 09:09:36 +0700

    Updaste

--- movieCRUD/app.js
+++ movieCRUD/app.js
@@ -20,6 +20,15 @@ var connectionString='mongodb://localhost:27017/'+dbName;
 
 mongoose.connect(connectionString);
 
+ allowCors = function(req, res, next) {
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+res.header('Access-Control-Allow-Headers', 'Content-Type');
+res.header('Access-Control-Allow-Credentials', 'true');
+next();
+};
+
+app.use(allowCors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
