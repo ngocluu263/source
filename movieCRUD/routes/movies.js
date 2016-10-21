0be7869d8c8055b644012908a143e11f37b306d3
luunguyen <luunguyen@ccintegration.com>
2016-10-21 09:10:48 +0700

    fdfd

--- movieCRUD/routes/movies.js
+++ movieCRUD/routes/movies.js
@@ -8,11 +8,7 @@ var express=require('express');
 //configure routes
 
 var router=express.Router();
-router.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-next();
-});
+
 router.route('/movies')
     .get(function(req,res){
        Movie.find(function(err,movies){
