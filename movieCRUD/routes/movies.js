b9e179503c2bb007be4c6847377ce66a5e8c8072
luunguyen <luunguyen@ccintegration.com>
2016-10-21 16:21:50 +0700

    45454

--- movieCRUD/routes/movies.js
+++ movieCRUD/routes/movies.js
@@ -9,7 +9,7 @@ var express=require('express');
 
 var router=express.Router();
 
-router.route('/movies')
+router.route('/movies1')
     .get(function(req,res){
        Movie.find(function(err,movies1){
            if(err)
@@ -27,7 +27,7 @@ router.route('/movies')
         });
     });
 
-router.route('/movies/:id')
+router.route('/movies1/:id')
     .put(function(req,res){
         Movie.findOne({_id:req.params.id},function(err,movie){
 
